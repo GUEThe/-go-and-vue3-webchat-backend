4f1f890704f524ffb6bfd1063d90eb686d99f64a
GUEThe <guethejq@gmail.com>
2024-06-03 19:40:18 +0800

    sss

--- main.go
+++ main.go
@@ -1,48 +1,24 @@
 package main
 
 import (
-	"fmt"
-	"io"
-
 	"github.com/GUEThe/go-and-vue3-webchat-backend/pkg/chat"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"golang.org/x/net/websocket"
 )
 
-func handleMsg(ws *websocket.Conn, c echo.Context) {
-	for {
-		// Write
-		err := websocket.Message.Send(ws, "Hello, Client!")
-		if err != nil {
-			c.Logger().Error(err)
-			break
-		}
-
-		// Read
-		msg := ""
-		err = websocket.Message.Receive(ws, &msg)
-		if err != nil {
-			c.Logger().Info(err == io.EOF)
-			c.Logger().Error(err)
-			break
-		}
-		fmt.Printf("%s\n", msg)
-	}
-}
-
-func wsHandler(c echo.Context) error {
-	// userWsMap := make(map[string]*websocket.Conn)
-	websocket.Handler(func(ws *websocket.Conn) {
-		client := chat.Client{}
-	}).ServeHTTP(c.Response(), c.Request())
-	return nil
-}
-
 func main() {
 	e := echo.New()
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
-	e.GET("/ws", wsHandler)
+	pool := chat.NewPool()
+	pool.init()
+	e.GET("/ws", func(c echo.Context) error {
+		websocket.Handler(func(ws *websocket.Conn) {
+			client := chat.Client{ID: "sss", EchoCtx: &c, Conn: ws, Pool: pool}
+			client.Read()
+		}).ServeHTTP(c.Response(), c.Request())
+		return nil
+	})
 	e.Logger.Fatal(e.Start(":1323"))
 }
--- pkg/chat/pool.go
+++ pkg/chat/pool.go
@@ -9,41 +9,41 @@ type Pool struct {
 	Broadcast  chan Message
 }
 
+func (pool *Pool) init() {
+	go func() {
+		for {
+			client := <-pool.Register
+			pool.Clients[client] = true
+			fmt.Println("Size of Connection Pool: ", len(pool.Clients))
+
+		}
+	}()
+	for {
+		select {
+		case client := <-pool.Register:
+			pool.Clients[client] = true
+			fmt.Println("Size of Connection Pool: ", len(pool.Clients))
+			// for client, _ := range pool.Clients {
+			// 	fmt.Println(client)
+			// 	client.Conn.WriteJSON(Message{Type: 1, Body: "New User Joined..."})
+			// }
+			break
+		case client := <-pool.Unregister:
+			delete(pool.Clients, client)
+			fmt.Println("Size of Connection Pool: ", len(pool.Clients))
+			// for client, _ := range pool.Clients {
+			// 	client.Conn.WriteJSON(Message{Type: 1, Body: "User Disconnected..."})
+			// }
+			break
+		}
+	}
+}
+
 func NewPool() *Pool {
 	return &Pool{
 		Register:   make(chan *Client),
 		Unregister: make(chan *Client),
 		Clients:    make(map[*Client]bool),
 		Broadcast:  make(chan Message),
-	}
-}
-
-func (pool *Pool) init() {
-	for {
-		select {
-		case client := <-pool.Register:
-			pool.Clients[client] = true
-			fmt.Println("Size of Connection Pool: ", len(pool.Clients))
-			for client, _ := range pool.Clients {
-				fmt.Println(client)
-				client.Conn.WriteJSON(Message{Type: 1, Body: "New User Joined..."})
-			}
-			break
-		case client := <-pool.Unregister:
-			delete(pool.Clients, client)
-			fmt.Println("Size of Connection Pool: ", len(pool.Clients))
-			for client, _ := range pool.Clients {
-				client.Conn.WriteJSON(Message{Type: 1, Body: "User Disconnected..."})
-			}
-			break
-		case message := <-pool.Broadcast:
-			fmt.Println("Sending message to all clients in Pool")
-			for client, _ := range pool.Clients {
-				if err := client.Conn.WriteJSON(message); err != nil {
-					fmt.Println(err)
-					return
-				}
-			}
-		}
 	}
 }
